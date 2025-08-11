b0919a6e11afd9e6b3b4958b1cffa4a236ac7d9b
relanse <relansenas@proton.me>
2025-08-11 20:43:19 +0800

    项目配置eslint和prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, node: true },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  }
+};
