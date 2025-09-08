0441b80e04bcf7233408257e548a569ee8f5b51e
relanse <relansenas@proton.me>
2025-09-08 11:23:35 +0800

    修改组件位置

--- apps/common/components/CourseTemplate/ChoicesTemplate.vue
+++ apps/common/components/CourseTemplate/ChoicesTemplate.vue
@@ -81,8 +81,8 @@ import {ElInput, ElRadio, ElRadioGroup, ElMessage} from 'element-plus'
 import backgroundGirl from '../../assets/backgrounds/backgroundGirl.png'
 import backgroundSun from '../../assets/backgrounds/backgroundSun.png'
 import MyButton from '../../../student-app/src/components/common/MyButton.vue'
-import EditableText from './EditableText.vue'
-import RobotPrompt from './RobotPrompt.vue'
+import EditableText from './components/EditableText.vue'
+import RobotPrompt from './components/RobotPrompt.vue'
 import {
   CourseTemplateProviderDefaultValue,
   CourseTemplateProviderKey
