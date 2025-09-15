4c1d60d583b4f414ac5290c833b559153c3fe5af
relanse <relansenas@proton.me>
2025-09-15 18:40:50 +0800

    'build'

--- apps/teacher-app/vite.config.ts
+++ apps/teacher-app/vite.config.ts
@@ -4,13 +4,15 @@ import ElementPlus from 'unplugin-element-plus/vite'
 //vue
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), ElementPlus({})],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@aipbl/common': path.resolve(__dirname, '../../common'),
     }
   },
   server: {
